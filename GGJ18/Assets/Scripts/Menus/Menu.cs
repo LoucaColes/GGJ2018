795bf2782e3609f14466e4167db557eeed94ae01
LoucaColes <coleslouca@yahoo.co.uk>
2018-01-27 10:57:57 +0000

    Menu Set Up

---
+++ GGJ18/Assets/Scripts/Menus/Menu.cs
@@ -0,0 +1,137 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using Rewired;
+
+public class Menu : MonoBehaviour
+{
+    public Button[] m_buttons;
+    private int m_buttonIndex;
+
+    public Color m_enabledColour;
+    public Color m_disabledColour;
+
+    public float m_moveSpeed;
+    private float m_timer;
+
+    private Player m_player;
+
+    // Use this for initialization
+    private void Start()
+    {
+        m_buttonIndex = 0;
+        m_player = ReInput.players.GetPlayer(0);
+        m_timer = 0;
+    }
+
+    // Update is called once per frame
+    private void Update()
+    {
+        InputHandling();
+        UpdateUI();
+    }
+
+    private void InputHandling()
+    {
+        m_timer += Time.deltaTime;
+        if (m_timer >= m_moveSpeed)
+        {
+            if (m_player.GetAxis("MoveVert") > 0 || m_player.GetAxis("MoveHor") > 0)
+            {
+                m_buttonIndex++;
+                if (m_buttonIndex >= m_buttons.Length)
+                {
+                    m_buttonIndex = 0;
+                }
+                m_timer = 0;
+            }
+            else if (m_player.GetAxis("MoveVert") < 0 || m_player.GetAxis("MoveHor") < 0)
+            {
+                m_buttonIndex--;
+                if (m_buttonIndex < 0)
+                {
+                    m_buttonIndex = m_buttons.Length - 1;
+                }
+                m_timer = 0;
+            }
+        }
+
+        if (m_player.GetButtonDown("Accept"))
+        {
+            switch (m_buttons[m_buttonIndex].name)
+            {
+                case "Game1Button":
+                    SceneLoader.m_instance.LoadGameOneMenu();
+                    break;
+
+                case "GameOnePlayButton":
+                    SceneLoader.m_instance.LoadGameOneCharacterSelect();
+                    break;
+
+                case "GameOneReturnButton":
+                case "GameTwoReturnButton":
+                    SceneLoader.m_instance.LoadMainMenu();
+                    break;
+
+                case "Game2Button":
+                    SceneLoader.m_instance.LoadGameTwoMenu();
+                    break;
+
+                case "GameTwoPlayButton":
+                    SceneLoader.m_instance.LoadGameTwoCharacterSelect();
+                    break;
+
+                case "ExitButton":
+                    SceneLoader.m_instance.ExitGame();
+                    break;
+            }
+        }
+
+        if (m_player.GetButtonDown("Back"))
+        {
+            switch (SceneLoader.m_instance.GetCurrentScene())
+            {
+                case "MainMenu":
+                    SceneLoader.m_instance.ExitGame();
+                    break;
+
+                case "GameOneMenu":
+                case "GameTwoMenu":
+                    SceneLoader.m_instance.LoadMainMenu();
+                    break;
+
+                case "GameOneCharacterSelect":
+                    SceneLoader.m_instance.LoadGameOneMenu();
+                    break;
+
+                case "GameTwoCharacterSelect":
+                    SceneLoader.m_instance.LoadGameTwoMenu();
+                    break;
+            }
+        }
+    }
+
+    private void UpdateUI()
+    {
+        for (int i = 0; i < m_buttons.Length; i++)
+        {
+            if (i == m_buttonIndex)
+            {
+                if (!m_buttons[i].enabled)
+                {
+                    m_buttons[i].enabled = true;
+                    m_buttons[i].image.color = m_enabledColour;
+                }
+            }
+            else
+            {
+                if (m_buttons[i].enabled)
+                {
+                    m_buttons[i].enabled = false;
+                    m_buttons[i].image.color = m_disabledColour;
+                }
+            }
+        }
+    }
+}
---
+++ GGJ18/Assets/Scripts/Menus/SceneLoader.cs
@@ -0,0 +1,67 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    public static SceneLoader m_instance;
+
+    // Use this for initialization
+    private void Start()
+    {
+        if (!m_instance)
+        {
+            m_instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+    }
+
+    public void LoadMainMenu()
+    {
+        SceneManager.LoadScene("MainMenu");
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void LoadGameOneMenu()
+    {
+        SceneManager.LoadScene("GameOneMenu");
+    }
+
+    public void LoadGameOneCharacterSelect()
+    {
+        SceneManager.LoadScene("GameOneCharacterSelect");
+    }
+
+    public void LoadGameOne()
+    {
+        SceneManager.LoadScene("GameOne");
+    }
+
+    public void LoadGameTwoMenu()
+    {
+        SceneManager.LoadScene("GameTwoMenu");
+    }
+
+    public void LoadGameTwoCharacterSelect()
+    {
+        SceneManager.LoadScene("GameTwoCharacterSelect");
+    }
+
+    public void LoadGameTwo()
+    {
+        SceneManager.LoadScene("GameTwo");
+    }
+
+    public string GetCurrentScene()
+    {
+        return SceneManager.GetActiveScene().name;
+    }
+}
